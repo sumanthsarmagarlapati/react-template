12649d7a4aa8581c7fcb2d6d3d36eaa3c8e95ce1
Sumanth Sarma <sumanthsarma1306@gmail.com>
2026-02-13 09:24:56 +0530

    initial commit

--- src/pages/Dashboard/dashboard.tsx
+++ src/pages/Dashboard/dashboard.tsx
@@ -1,5 +1,16 @@
+import { useEffect } from 'react';
+import axios from 'axios';
+
 export default function Dashboard() {
   // throw new Response("Dashboard crashed intentionally");
+    useEffect(() => {
+    axios
+      .get('https://dummyjson.com/users')
+      .then((res) => {
+        console.log('data', res?.data?.users);
+      })
+      .catch((err) => {});
+  }, []);
 
   return <div>Dashboard</div>;
 }
